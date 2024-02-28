db4ebc29a89238184310d4744289dbbb18bb9c2a
sikwan <yysskk99999@naver.com>
2024-02-28 21:19:23 +0900

    init

---
+++ weather_LocationApi_Test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather_LocationApi_Test
+//
+//  Created by 양시관 on 2/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather_LocationApi_Test/weather_LocationApi_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_LocationApi_TestApp.swift
+//  weather_LocationApi_Test
+//
+//  Created by 양시관 on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct weather_LocationApi_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
